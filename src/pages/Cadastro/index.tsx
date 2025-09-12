ed32bae3d05ec5d1e2b65c6e198621fca0c64d28
sulivan7 <devsulivan@gmail.com>
2025-09-12 16:11:50 -0300

    first commit

---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../containers/BarraLateral'
+import Formulario from '../../containers/Formulario'
+
+const Cadrastro = () => (
+  <>
+    <BarraLateral mostrarFiltros={false} />
+    <Formulario />
+  </>
+)
+
+export default Cadrastro
